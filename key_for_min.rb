30fcab4d8adddc98cb15f1afe45bb003edc674b1
nathan hains <nathanhains14@yahoo.com>
2021-02-05 14:31:27 -0500

    commit tree 30b3d8c29a0c1a3c039bff02e03bc66e1bbf0f76

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    winner = 0
+    name_hash.collect do |name, price|
+        if winner == 0
+            winner = price
+        elsif price < winner 
+            winner = price
+        end
+    end
+    name_hash.index(winner)
 end
